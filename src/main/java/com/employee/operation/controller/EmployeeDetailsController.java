6d397933b9e172d42781f6c88083ad347abdbc91
kanki154 <kankipatiashok123@gmail.com>
2024-01-24 15:14:32 +0530

    adding id logic

--- src/main/java/com/employee/operation/controller/EmployeeDetailsController.java
+++ src/main/java/com/employee/operation/controller/EmployeeDetailsController.java
@@ -1,10 +1,17 @@
 package com.employee.operation.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Optional;
+
 import com.employee.operation.entity.EmployeeDetails;
 import com.employee.operation.repo.EmployeeDetailRepo;
+
+
 
 @RestController
 public class EmployeeDetailsController {
@@ -24,6 +31,21 @@ public class EmployeeDetailsController {
 	public org.springframework.http.ResponseEntity<List<EmployeeDetails>> getEmployeeDetails() {
 		
 	return  new org.springframework.http.ResponseEntity<>( employeetrepo.findAll(),HttpStatus.OK);
+		
+	}
+	
+	@GetMapping("/api/employee/{id}")
+	public ResponseEntity<EmployeeDetails> getStudentDetailsById(@PathVariable long id) {
+		
+		Optional<EmployeeDetails> student= employeetrepo.findById(id);
+		if(student.isPresent())
+		{
+			return  new ResponseEntity<>(student.get(),HttpStatus.OK);
+		} 
+		else {
+			return  new ResponseEntity<>( HttpStatus.NOT_FOUND);
+		}
+	
 		
 	}
 
